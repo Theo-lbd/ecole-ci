73bcf43d14731ead0e759cdc6924cad7160fdb19
Theo Labadie <theo.labadie@fms-ea.com>
2025-06-02 13:02:18 +0200

    Correction de la méthode __str__ de Teacher pour inclure l'âge

--- ecole/teacher.py
+++ ecole/teacher.py
@@ -26,4 +26,4 @@ class Teacher(Person):
 
     def __str__(self) -> str:
         person_str = super().__str__()
-        return f"{person_str}, arrivé(e) le {self.hiring_date}"
+        return f"{self.first_name} {self.last_name} ({self.age} ans), arrivé(e) le {self.hiring_date}"
